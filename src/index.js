a95ef7cc5b2c6e99ee931e563f43a78600a5a0f8
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-02-28 03:19:27 -0300

    chore: adjust code for deploy on render

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ app.use(usersRoutes);
 app.use(urlsRoutes);
 app.use(usersUrlsRoutes);
 
-
-app.listen(5000, console.log(`runing in port ${5000}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
