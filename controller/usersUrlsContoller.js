d1bb818c7205771f3513f0daac5fbb99f871612b
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-03-03 00:42:43 -0300

    refactory2: GET Ranking

--- controller/usersUrlsContoller.js
+++ controller/usersUrlsContoller.js
@@ -1,14 +1,15 @@
 import db from "../database/db.js";
 
 
-export async function getUserMe(){
+export async function getUserMe(req, res){
+
 
 }
 
 export async function getRanking(req, res){
 
     try{
-        const rankeada = await db.query(`SELECT users.id, users.name, COUNT(urls) AS "linksCount", SUM(urls."visitCount") AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10`);
+        const rankeada = await db.query(`SELECT users.id, users.name, COUNT(urls.url) AS "linksCount", SUM(urls."visitCount") AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10`);
 
         res.status(200).send(rankeada.rows);
     }catch(err){
